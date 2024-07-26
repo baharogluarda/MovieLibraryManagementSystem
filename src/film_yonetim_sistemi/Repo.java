4de8bd4c7c1654dcc3f7ae75a73917666906897b
Arda Baharoğlu <arda.900bhrgl@gmail.com>
2024-07-26 16:05:18 +0300

    .

--- src/film_yonetim_sistemi/Repo.java
+++ src/film_yonetim_sistemi/Repo.java
@@ -1,10 +1,12 @@
 package film_yonetim_sistemi;
 
+import java.util.List;
+
 public interface Repo <T>{
 
     void add(T t);
     void delete(int id);
-    void update(T t);
+    void updateRating(int id);
     void get();
 
 
